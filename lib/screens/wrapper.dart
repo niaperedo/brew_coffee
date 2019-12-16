f93fa829d822b833f11642bcebda65e691faf0a1
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-16 11:53:42 +0800

    Add lesson 8

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
 import 'package:brew_coffee/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_coffee/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
     return Authenticate();
   }
 }
