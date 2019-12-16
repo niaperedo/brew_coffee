a9d5591e696b19a9b94a6e8cd9a06472a8838f68
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-16 09:57:58 +0800

    Add lesson 4

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,9 @@
-import 'package:brew_coffee/screens/home/home.dart';
+import 'package:brew_coffee/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return Authenticate();
   }
 }
