bca76c72e0d8ea6a915331ec6bd7caa82424b56a
Ian Peredo <aperedo.uth702@gmail.com>
2019-12-18 13:56:17 +0800

    Add lesson 22

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:brew_coffee/screens/home/settings_form.dart';
 import 'package:brew_coffee/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_coffee/services/database.dart';
@@ -16,7 +17,7 @@ class Home extends StatelessWidget {
         builder: (context) {
           return Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-            child: Text('bottom sheets'),
+            child: SettingsForm()
           );
         }
       );
